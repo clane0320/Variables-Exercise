ae3d9746c97c8b68c8549dbcc8f33889d31fa6b9
Chris Lane <clane0320@gmail.com>
2022-12-07 21:27:39 -0600

    declared and intialized variables

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -1,10 +1,10 @@
-﻿namespace VariablesExercise
-{
-    public class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿// Declare and Initialize Variables
+string name = "Trinity";
+    int age = 21;
+char middleIntial = 'M';
+bool isPregnant = true;
+double currentWeek = 25;
+decimal currentPrice = 3500.00m;
+
+Console.WriteLine($"My girlfriends name is {name}, she is {age}. Her middle intial is {middleIntial}.");
+Console.WriteLine($"It is {isPregnant} that she is pregnant and is at {currentWeek} weeks. It is going to cost us {currentPrice} dollars.");
